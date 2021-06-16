ccc85c8b8a7db07fb748347451a28939a4b17621
D3h4n <shawn2012tt@live.com>
2021-06-16 19:50:01 -0400

    Initial Commit

---
+++ Calculator.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class Calculator {
+  public static void main(String[] args) {
+    final Scanner stdin = new Scanner(System.in);
+
+    System.out.print("Enter an expression: ");
+    String input = stdin.nextLine();
+    
+    while (input.compareTo("end") != 0) {
+      double result = parseExpression(input);
+      System.out.println("Result: " + result + "\n");
+      
+      System.out.print("Enter an expression: ");
+      input = stdin.nextLine();
+    }
+    
+    stdin.close(); // close scanner
+  }
+
+  static double parseExpression(String expression) {
+    final String[] operations = {"-", "+", "*", "/", "^"};
+    String[] operands;
+    String operation = "";
+    expression = removeSpaces(expression);
+    
+    for (String opp: operations) {
+      if (expression.contains(opp)) {
+        operation = opp;
+        break;
+      }
+    }
+
+    if (operation.isBlank()) {
+      return expression.isBlank() ? 0 : Double.valueOf(expression);
+    }
+    else {
+      operands = splitByOperation(expression, operation);
+
+      switch(operation) {
+        case "-":
+          return parseExpression(operands[0]) - parseExpression(operands[1]);
+
+        case "+":
+          return parseExpression(operands[0]) + parseExpression(operands[1]);
+
+        case "*":
+          return parseExpression(operands[0]) * parseExpression(operands[1]);
+
+        case "/":
+          return parseExpression(operands[0]) / parseExpression(operands[1]);
+        
+        case "^":
+          return Math.pow(parseExpression(operands[0]),  parseExpression(operands[1]));
+      }
+
+      return Integer.MAX_VALUE;
+    }
+  }
+
+  static String removeSpaces(String input) {
+    return input.replaceAll(" ", "");
+  }
+  
+  static String[] splitByOperation(String expression, String operation) {
+    String[] result = {"", ""};
+    
+    int position = expression.lastIndexOf(operation);
+  
+    result[0] = expression.substring(0, position);
+    result[1] = expression.substring(position + 1);
+    
+    return result;
+  }
+}
