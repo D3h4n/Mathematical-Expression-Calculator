7cfbc2ec41cc813a6b8ba3f8a6e1bfab81fa1a8a
D3h4n <shawn2012tt@live.com>
2021-06-16 23:51:01 -0400

    Add Comments and readme

--- Calculator.java
+++ Calculator.java
@@ -2,28 +2,38 @@ import java.util.Scanner;
 
 public class Calculator {
   public static void main(String[] args) {
-    final Scanner stdin = new Scanner(System.in);
+    final Scanner stdin = new Scanner(System.in); // scanner for user input
 
-    System.out.print("Enter an expression: ");
+    // get user input
+    System.out.print("Enter an expression:(end) ");
     String input = stdin.nextLine();
     
-    while (input.compareTo("end") != 0) {
-      double result = calculateExpression(input);
-      System.out.println("Result: " + result + "\n");
+    while (!input.isBlank()) { // loop while user inputs an expression
+      double result = calculateExpression(input); // calculate value of expression
+      System.out.println("Result: " + result + "\n"); // output value
       
-      System.out.print("Enter an expression: ");
+      // get user input
+      System.out.print("Enter an expression:(end) ");
       input = stdin.nextLine();
     }
     
     stdin.close(); // close scanner
   }
 
+
+  /**
+   * Calculate the value of the expression
+   * 
+   * @param expression - expression to be calculated
+   * @return value of the expression
+   */
   static double calculateExpression(String expression) {
-    final String[] operations = {"-", "+", "*", "/", "^"};
-    String[] operands;
-    String operation = "";
-    expression = removeSpaces(expression);
+    final String[] operations = {"+", "-", "*", "/", "^"}; // possible operatations in order of precedence 
+    String operation = ""; // string to store operation
     
+    expression = expression.replaceAll(" ", ""); // remove all whitespace from expression
+
+    // iterate to find operand with most precendence in expression
     for (String opp: operations) {
       if (expression.contains(opp)) {
         operation = opp;
@@ -31,14 +41,20 @@ public class Calculator {
       }
     }
 
-    if (operation.isBlank()) {
+    if (operation.isBlank()) { 
+      // if no operand is found return value of expression
       return expression.isBlank() ? 0 : Double.valueOf(expression);
     }
     else {
-      operands = splitByOperation(expression, operation);
+      String[] operands = splitByOperation(expression, operation); // split string into the two operands of the operation
 
+      // perform operation on operands and return result
       switch(operation) {
         case "-":
+          if (operands[0].isEmpty()) {
+            return -calculateExpression(operands[1]);
+          }
+
           return calculateExpression(operands[0]) - calculateExpression(operands[1]);
 
         case "+":
@@ -57,17 +73,24 @@ public class Calculator {
       return Integer.MAX_VALUE;
     }
   }
-
-  static String removeSpaces(String input) {
-    return input.replaceAll(" ", "");
-  }
   
+  /**
+   * Split the operands of a binary operator 
+   * 
+   * @param expression - expression to separate
+   * @param operation - operation to separate by
+   * @return array of strings with separated operands
+   */
   static String[] splitByOperation(String expression, String operation) {
     String[] result = {"", ""};
     
+    // get index of operation
     int position = expression.lastIndexOf(operation);
-  
+
+    // split first half
     result[0] = expression.substring(0, position);
+    
+    // split second half
     result[1] = expression.substring(position + 1);
     
     return result;
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Java Expression Calculator
+
+## About
+
+Simple Java program to parse and calculate the value of  
+a mathematical expression.
+
+## Todo
+
+- [] Implement Parentheses
+- [x] Account for negative numbers
